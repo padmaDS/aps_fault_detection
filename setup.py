5b467cc35cb52187a0e9507e9235dcb698daf2d3
padmaDS <paddumba10@gmail.com>
2023-07-25 13:44:57 +0530

    requirements.txt

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+
+def get_requirements():
+    pass
+
+
+
+setup = (
+    name='sensor',
+    version = '0.0.1',
+    author = 'padma'
+    author_email = 'paddumba10@gmail.com',
+    packages = find_packages(),
+    install_requires = get_requirements()
+
+
+
+)
